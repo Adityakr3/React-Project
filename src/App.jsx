4d1ac889c79941e15fd171d48f97046312adea08
Aditya kumar <adityac22f@gmail.com>
2024-12-11 12:14:56 +0530

    connect BrowserRouter , Routes , Route all comp with App.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,29 @@ import React from "react";
 import "./index.css"
 // import Child1 from "./Child1";
 // import UseReducerHook from "./useReducer/UseReducerHook";
-
-import DynamicForm from "./DynamicForm/DynamicForm";
 import Game from "./Moving Game/Game";
-// import Game from "./Moving Game/Game";
-
-import {BackGroundChange} from "./BackGroundChange/BackGroundChange.jsx";
-
-
+import { BackGroundChange } from "./BackGroundChange/BackGroundChange.jsx";
+import LandingPage from "./LandingPage/LandingPage.jsx";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import Signup from "./LandingPage/signUp.jsx";
 const App = () => {
-  return (
-   <>
-    {/* <Child1/>
-    <UseReducerHook/> */}
-    {/* <Game/> */}
-    <DynamicForm/>
-    {/* <BackGroundChange/> */}
-   </>
-  );
+
+   const NavStyle = {
+      color: "white"
+   }
+   return (
+      <BrowserRouter>
+        <div className="Nav">
+        </div>
+         <Routes>
+            <Route path="/background" element={<BackGroundChange />} ></Route>
+            <Route path="/" element={<LandingPage />} ></Route>
+            <Route path="/Game" element={<Game />} ></Route>
+            <Route path="/sign" element={<Signup />} ></Route>
+         </Routes>
+      </BrowserRouter>
+
+   );
 };
 
 export default App;
